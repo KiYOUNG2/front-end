21d13eff8821e7d376daafe6e6c1b77d039242a8
Haram Lee <twinha0204@gmail.com>
2021-12-10 04:44:44 +0000

    [#3] Fix the chat window size and add scrolling

--- src/components/chat/Message.vue
+++ src/components/chat/Message.vue
@@ -1,32 +1,34 @@
 <template>
-  <v-container class="fill-height">
-    <v-row class="fill-height pb-14" align="end">
-      <v-col>
-        <div
-          v-for="(item, index) in chat"
-          :key="index"
-          :class="[
-            'd-flex flex-row align-center my-2',
-            item.from == 'user' ? 'justify-end' : null,
-          ]"
-        >
-          <span v-if="item.from == 'user' && item.msg != null" class="blue--text mr-3" style="max-width: 60%; text-align: right;">{{
-            item.msg
-          }}</span>
-          <ZoomImage v-if="item.from == 'user' && item.img != null" v-bind:item="item" />
-          <v-avatar
-            :color="item.from == 'user' ? 'grey' : 'amber accent-2'"
-            size="36"
+  <v-container>
+    <section style="height: 700px; overflow-y: auto; overflow-x: hidden;">
+      <v-row align="end" style="padding: 1%;">
+        <v-col>
+          <div
+            v-for="(item, index) in chat"
+            :key="index"
+            :class="[
+              'd-flex flex-row align-center my-2',
+              item.from == 'user' ? 'justify-end' : null,
+            ]"
           >
-            <span class="white--text">{{ item.from[0] }}</span>
-          </v-avatar>
-          <span v-if="item.from != 'user' && item.msg != null" class="blue--text ml-3" style="max-width: 60%; text-align: left;">{{
-            item.msg
-          }}</span>
-          <ZoomImage v-if="item.from != 'user' && item.img != null" v-bind:item="item" />
-        </div>
-      </v-col>
-    </v-row>
+            <span v-if="item.from == 'user' && item.msg != null" class="blue--text mr-3" style="max-width: 60%; text-align: right;">{{
+              item.msg
+            }}</span>
+            <ZoomImage v-if="item.from == 'user' && item.img != null" v-bind:item="item" />
+            <v-avatar
+              :color="item.from == 'user' ? 'grey' : 'amber accent-2'"
+              size="36"
+            >
+              <span class="white--text">{{ item.from[0] }}</span>
+            </v-avatar>
+            <span v-if="item.from != 'user' && item.msg != null" class="blue--text ml-3" style="max-width: 60%; text-align: left;">{{
+              item.msg
+            }}</span>
+            <ZoomImage v-if="item.from != 'user' && item.img != null" v-bind:item="item" />
+          </div>
+        </v-col>
+      </v-row>
+    </section>
   </v-container>
 </template>
 
