cc775af318360385069a5eb62ab0319c4e1f0ed7
Haram Lee <twinha0204@gmail.com>
2021-12-10 06:13:55 +0000

    [#3] Change the profile of the chat window

--- src/components/chat/Message.vue
+++ src/components/chat/Message.vue
@@ -1,45 +1,53 @@
 <template>
   <v-container>
     <section style="height: 700px; overflow-y: auto; overflow-x: hidden">
-      <v-row align="end" style="padding: 1%">
-        <v-col>
-          <div
-            v-for="(item, index) in chat"
-            :key="index"
-            :class="[
-              'd-flex flex-row align-center my-2',
-              item.from == 'user' ? 'justify-end' : null,
-            ]"
-          >
+      <div
+        align="end"
+        style="padding: 1%"
+        v-for="(item, index) in chat"
+        :key="index"
+        :class="[
+          'd-flex flex-row my-2',
+          item.from == 'user' ? 'justify-end' : null,
+        ]"
+      >
+        <v-row class="flex-column" v-if="item.from == 'user'">
+          <v-col :class="['d-flex child-flex', 'justify-end']">
+            <span>{{ item.from }}</span>
+          </v-col>
+          <v-col :class="['d-flex child-flex', 'justify-end']">
             <span
-              v-if="item.from == 'user' && item.msg != null"
+              v-if="item.msg != null"
               class="blue--text mr-3"
               style="max-width: 60%; text-align: right"
               >{{ item.msg }}</span
             >
-            <ZoomImage
-              v-if="item.from == 'user' && item.img != null"
-              v-bind:item="item"
-            />
-            <v-avatar
-              :color="item.from == 'user' ? 'grey' : 'amber accent-2'"
-              size="36"
-            >
-              <span class="white--text">{{ item.from[0] }}</span>
-            </v-avatar>
+            <ZoomImage v-if="item.img != null" v-bind:item="item" />
+          </v-col>
+        </v-row>
+        <v-col cols="1">
+          <v-avatar
+            :color="item.from == 'user' ? 'grey' : 'amber accent-2'"
+            size="50"
+          >
+            <v-img :src="getProfileImage(item.from)"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-row class="flex-column" v-if="item.from != 'user'">
+          <v-col class="d-flex child-flex">
+            <span style="text-align: left">{{ item.from }}</span>
+          </v-col>
+          <v-col class="d-flex child-flex">
             <span
-              v-if="item.from != 'user' && item.msg != null"
+              v-if="item.msg != null"
               class="blue--text ml-3"
               style="max-width: 60%; text-align: left"
               >{{ item.msg }}</span
             >
-            <ZoomImage
-              v-if="item.from != 'user' && item.img != null"
-              v-bind:item="item"
-            />
-          </div>
-        </v-col>
-      </v-row>
+            <ZoomImage v-if="item.img != null" v-bind:item="item" />
+          </v-col>
+        </v-row>
+      </div>
     </section>
   </v-container>
 </template>
@@ -53,5 +61,14 @@ export default {
     ZoomImage,
   },
   props: ["chat"],
+  methods: {
+    getProfileImage(from) {
+      if (from == "user") {
+        return require("@/assets/image/profile_bart.png");
+      } else {
+        return require("@/assets/image/profile_kiyoung.png");
+      }
+    },
+  },
 };
 </script>
