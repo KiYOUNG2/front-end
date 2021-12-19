936bda5a461fa14ec59bf809fafe2e00c4becd90
Haram Lee <twinha0204@gmail.com>
2021-12-19 01:10:38 +0000

    [#4] Add navigation and about page

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <Chat />
+</template>
+
+<script>
+import Chat from "../components/chat/Chat.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Chat,
+  },
+};
+</script>
